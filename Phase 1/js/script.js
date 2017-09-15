e7af8642a26fbf3e95b516cd0dde542b18823aea
Sujoy Roy <sujoyroy689@gmail.com>
2017-09-15 15:55:14 +0530

    Add files via upload

---
+++ Phase 1/js/script.js
@@ -0,0 +1,26 @@
+// This is for List and Grid 
+
+listButton = $('button.list-view');
+gridButton = $('button.grid-view');
+wrapper = $('div.wrapper');
+
+
+
+listButton.on('click',function(){
+	
+  gridButton.removeClass('on');
+  listButton.addClass('on');
+  wrapper.removeClass('grid').addClass('list');
+  
+});
+
+gridButton.on('click',function(){
+	
+  listButton.removeClass('on');
+  gridButton.addClass('on');
+  wrapper.removeClass('list').addClass('grid');
+  
+});
+
+//END
+
