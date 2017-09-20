418da85dd52bb0e7ca8c5d99694118d2e91653f4
Sujoy Roy <sujoyroy689@gmail.com>
2017-09-20 20:47:10 +0530

    add shopping cart functionality

--- js/script.js
+++ js/script.js
@@ -27,22 +27,34 @@ gridButton.on('click',function(){
 // This is For Adding to Cart and Calculating total Billing Amount
 
 var total=0;
-    var counter = 1;
+    var counter = 0;
 	function myFunction(value, name)
 	{
 
 
 	if(counter<10){
     	counter++;
+    $('#item-count').html(counter);
 		total+=value;
 		console.log(total);
 		 document.getElementById("test").innerHTML=total;
-		 $(".product").append("<p>"+name+"</p>");
-
-		 $(".product").append("<p>"+value+"</p><hr>");
-		}
+		 $(".product").append("<span>"+name+"<br>"+value+"\
+                          <i id='cancel-btn-"+counter+"' class='fa fa-times' aria-hidden='true' style='float: right; display:inline;' onclick=removeItem('#cancel-btn-"+counter+"',"+ value+")></i><hr>\
+                        </span>");
+  }
 	else{
 		alert("you can't add more than nine")
 		}
 	}
+function removeItem(ele, value){
+  // console.log(value);
+  $(ele).parent().remove();
+  if(counter > 0){
+    total-=value;
+    counter--;
+  }
+  console.log(total);
+  document.getElementById("test").innerHTML=total;
+  $('#item-count').html(counter);
+} 
 //END
